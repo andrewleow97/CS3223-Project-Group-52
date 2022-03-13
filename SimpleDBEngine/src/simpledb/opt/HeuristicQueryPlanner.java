9d2542c8ce36c5649e4929163bf5366131f282b5
JasonLeeWeiHern <e0309651@u.nus.edu>
2022-03-13 16:12:25 +0800

    fix query plan

--- SimpleDBEngine/src/simpledb/opt/HeuristicQueryPlanner.java
+++ SimpleDBEngine/src/simpledb/opt/HeuristicQueryPlanner.java
@@ -138,9 +138,7 @@ public class HeuristicQueryPlanner implements QueryPlanner {
 				String table = queryPlan.get("table").get(i);
 				//If there is no index used at all
 				//TODO: Must we include a condition that if the join is not IndexJoin, we do not put anything for the index?
-				if (queryPlan.get("index").size() == 0) {
-					s += "(scan " + table + ")";
-				} else {
+				try {
 					String fldname = queryPlan.get("index").get(i*2);
 					String indexType = queryPlan.get("index").get((i*2)+1);
 					if(indexType != "empty") {
@@ -148,6 +146,8 @@ public class HeuristicQueryPlanner implements QueryPlanner {
 					} else {
 						s += "(scan " + table + ")";
 					}
+				} catch (IndexOutOfBoundsException e) {
+					s += "(scan " + table + ")";
 				}
 				
 				//if is not the last table
@@ -230,7 +230,6 @@ public class HeuristicQueryPlanner implements QueryPlanner {
 	private Plan getLowestJoinPlan(Plan current) {
 		TablePlanner besttp = null;
 		Plan bestplan = null;
-		
 		for (TablePlanner tp : tableplanners) {
 			
 			// if non-equi join{ bestplan = nestedloopplan}
@@ -246,7 +245,7 @@ public class HeuristicQueryPlanner implements QueryPlanner {
 			Plan sortMergePlan = tp.makeSortMergePlan(current);
 			Plan nestedLoopPlan = tp.makeNestedLoopPlan(current);
 			Plan hashJoinPlan = tp.makeHashJoinPlan(current);
-			
+			System.out.println(queryPlan);
 			if(sortMergePlan == null && nestedLoopPlan == null && hashJoinPlan == null) {
 				Plan productPlan = tp.makeDefaultProductPlan(current);
 				bestplan = productPlan;
